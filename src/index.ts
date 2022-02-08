f8f935e39ddace7081ff20fbb40176e78736b59f
Joel Pelaez Jorge <joel.pelaez.jorge@gmail.com>
2022-02-08 16:13:44 -0500

    remove blank lines

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ import { CWElement } from "./CWElement";
 import { CWPriority } from "./Enums";
 import * as Callbacks from "./EventCallbacks";
 
-
 export { CWLayer, CWElement, CWPriority, Callbacks }
